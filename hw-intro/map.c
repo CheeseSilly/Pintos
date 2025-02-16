58bb5fb148e90df9c95c2f250beaf7cb88bc646f
CheeseSilly <si11ycheese6680@gmail.com>
2025-02-16 08:09:15 +0000

    finish HW-0(INTRO)

--- hw-intro/map.c
+++ hw-intro/map.c
@@ -7,21 +7,20 @@ int foo;
 /* UNCOMMENT THIS LINE for 3.4.3*/
 extern int recur(int i);
 
-
 /* A statically allocated, pre-initialized variable */
 volatile int stuff = 7;
 
-int main(int argc, char *argv[]) {
-    /* A stack allocated variable */
-    volatile int i = 0;
-    
-    #include "printer.c"
+int main(int argc, char* argv[]) {
+  /* A stack allocated variable */
+  volatile int i = 0;
 
-    /* Dynamically allocate some stuff */
-    volatile char *buf1 = malloc(100);
-    /* ... and some more stuff */
-    volatile char *buf2 = malloc(100);
+#include "printer.c"
 
-    recur(3);
-    return 0;
+  /* Dynamically allocate some stuff */
+  volatile char* buf1 = malloc(100);
+  /* ... and some more stuff */
+  volatile char* buf2 = malloc(100);
+
+  recur(3);
+  return 0;
 }
